f9135d553e8bc4473b91e5b1b2139b8039733c47
Stephen Wright <scw248@gmail.com>
2019-01-29 17:52:08 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -12,6 +12,6 @@ class Backer
   def back_project(project)
     @backed_projects << project
     #binding.pry
-    project.add_backer(self) unless project.add_backer.include?(self)
+    project.add_backer(self) 
   end
 end
